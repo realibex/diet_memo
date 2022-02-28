d20501e601b4501dccbca5c9dbbda288a32929f0
realibex <ibex97@naver.com>
2022-02-28 16:42:25 +0900

    uid를 활용한 개인화

--- app/src/main/java/com/example/diet_memo/MainActivity.kt
+++ app/src/main/java/com/example/diet_memo/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -31,7 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         listView.adapter = adapter_list
 
-        myRef.addValueEventListener(object : ValueEventListener{
+        myRef.child(Firebase.auth.currentUser!!.uid).addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 dataModelList.clear()
 
@@ -89,7 +90,8 @@ class MainActivity : AppCompatActivity() {
                 val healthMemo = mAlertDialog.findViewById<EditText>(R.id.healthMemo)?.text.toString()
 
                 val database = Firebase.database
-                val myRef = database.getReference("myMemo")
+                val myRef = database.getReference("myMemo").child(Firebase.auth.currentUser!!.uid)
+
                 val model = DataModel(dateText, healthMemo)
 
                 myRef
